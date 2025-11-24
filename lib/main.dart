18ba0a31df780e49be0551be07df061467964d6f
Paulo Roberto De Morais <prmorais1302@gmail.com>
2025-11-24 16:32:11 -0300

    Create second page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,88 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/pages/home_page.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(const Quiz());
+}
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: Center(
+            child: Text(
+              'Quiz - Curso Flutter & Dart',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text('Pergunta 1 de 10'),
+              Text('Pergunta'),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  padding: EdgeInsets.all(10),
+                  fixedSize: Size(200, 60),
+                ),
+                child: Text(
+                  'Resposta 1',
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  padding: EdgeInsets.all(10),
+                  fixedSize: Size(200, 60),
+                ),
+                child: Text(
+                  'Resposta 2',
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  padding: EdgeInsets.all(10),
+                  fixedSize: Size(200, 60),
+                ),
+                child: Text(
+                  'Resposta 3',
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  padding: EdgeInsets.all(10),
+                  fixedSize: Size(200, 60),
+                ),
+                child: Text(
+                  'Resposta 4',
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
